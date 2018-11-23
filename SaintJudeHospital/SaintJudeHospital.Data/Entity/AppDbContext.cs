46f91c9f2c60f3a7e7bed7b4e28516efb9b91c96
dharl072810 <dharl072810@gmail.com>
2018-11-23 15:35:32 +0800

    Add mediatR and related libraries. Add initial migration.

---
+++ SaintJudeHospital/SaintJudeHospital.Data/Entity/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SaintJudeHospital.Data.Entity
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { set; get; }
+
+        public DbSet<Role> Roles { set; get; }
+    }
+}
---
+++ SaintJudeHospital/SaintJudeHospital.Data/Entity/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace SaintJudeHospital.Data.Entity
+{
+    public class User
+    {
+        public int UserId { set; get; }
+        public string FirstName { set; get; }
+        public string LastName { set; get; }
+        public string Username { set; get; }
+        public string Password { set; get; }
+        public int RoleId { set; get; }
+        public Role Role { set; get; }
+    }
+}
