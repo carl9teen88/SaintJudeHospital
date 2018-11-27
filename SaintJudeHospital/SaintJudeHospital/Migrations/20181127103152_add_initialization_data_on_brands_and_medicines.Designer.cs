7c91f17a1ea8c17b3b2747064df4570d83f29bea
carl9teen88 <dharl072810@gmail.com>
2018-11-27 18:33:43 +0800

    Finalized data on medicines and brands

--- SaintJudeHospital/SaintJudeHospital/Migrations/20181127103152_add_initialization_data_on_brands_and_medicines.Designer.cs
+++ SaintJudeHospital/SaintJudeHospital/Migrations/20181127103152_add_initialization_data_on_brands_and_medicines.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SaintJudeHospital.Data.Entity;
 
 namespace SaintJudeHospital.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20181127103152_add_initialization_data_on_brands_and_medicines")]
+    partial class add_initialization_data_on_brands_and_medicines
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
